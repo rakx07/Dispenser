9fc4ea0498faf641a40d081ca66d61d00082076d
Janry S <janryseran069407@gmail.com>
2024-06-30 23:29:28 +0800

    modal alert not functioning

--- dispenser/app/Http/Controllers/StudentController.php
+++ dispenser/app/Http/Controllers/StudentController.php
@@ -128,6 +128,6 @@ class StudentController extends Controller
         $studentUser->save();
 
         // return redirect()->route('voucher')->with('message', 'Account created successfully.');
-        return redirect()->route('signin');
+        return redirect()->route('signin')->with('message', 'Account created successfully.');
     }
 }
