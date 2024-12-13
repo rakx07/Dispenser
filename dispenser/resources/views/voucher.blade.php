66f14150a6501de96794caa63c0e475a6460aa0d
janry <janryseran069407@gmail.com>
2024-12-13 15:55:02 +0800

    added Note to voucher code

--- dispenser/resources/views/voucher.blade.php
+++ dispenser/resources/views/voucher.blade.php
@@ -76,6 +76,7 @@
                 <p><strong>SATP Username:</strong> {{ $student->school_id }}</p>
                 <p><strong>SATP Password:</strong> {{ $satp_password }}</p>
                 <p><strong>SATP Link:</strong> <span>http://satp.ndmu.edu.ph</span></p>
+                <p><strong>NOTE: <span>Use Google Chrome Web Browser.</span></strong></p>
 
 
             </div>
