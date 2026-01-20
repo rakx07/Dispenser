60d53378657982e5f6aa71fb30a3b0d6f6ef47a6
janry <janryseran069407@gmail.com>
2026-01-20 14:54:12 +0800

    test2

--- dispenser/resources/views/welcome.blade.php
+++ dispenser/resources/views/welcome.blade.php
@@ -78,7 +78,7 @@
     <main class="container mt-5">
         <!-- Beating Text -->
         <div class="mb-5">
-            <p class="beating-text">GET YOUR INSTITUTIONAL EMAIL, SCHOOLOGY, SATP, AND WIFI VOUCHER CODE HERE2!</p>
+            <p class="beating-text">GET YOUR INSTITUTIONAL EMAIL, SCHOOLOGY, SATP, AND WIFI VOUCHER CODE HERE!</p>
         </div>
 
         <div class="row">
